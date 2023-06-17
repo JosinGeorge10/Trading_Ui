7bbbe46b6800d02128a7e8ef8e101f55d58a6717
JosinGeorge10 <josingkollam@gmail.com>
2023-06-17 16:52:36 +0530

    traderprofile component modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import styled from "styled-components";
 import NavBar from "./Components/Includes/NavBar";
 import Header from "./Components/Includes/Header";
 import Demo from "./Components/Screens/Demo";
+import TraderProfile from "./Components/Screens/TraderProfile";
 import Echart from "./Components/Screens/Echart";
 import MailBox from "./Components/Screens/MailBox";
 import Setting from "./Components/Screens/Setting";
 import Pages from "./Components/Screens/Pages";
+import Support from "./Components/Screens/Support";
 
 
 function App() {
@@ -18,10 +20,12 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Demo />} />
+          <Route path="/traderprofile" element={<TraderProfile />} />
           <Route path="/echart" element={<Echart />} />
           <Route path="/mailbox" element={<MailBox />} />
           <Route path="/settings" element={<Setting />} />
           <Route path="/pages" element={<Pages />} />
+          <Route path="/support" element={<Support />} />
         </Routes>
       </Conatiner>
     </Router>
---
+++ src/Components/Screens/Support.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Support() {
+  return (
+    <div>
+      <h3 style={{color:"white"}}>Coming Soon......</h3>
+    </div>
+  );
+}
+
+export default Support;

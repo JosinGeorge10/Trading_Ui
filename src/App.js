8cdd5b663ced74d73d916ef8bb8c9eb45c39eebd
JosinGeorge10 <josingkollam@gmail.com>
2023-06-17 14:24:27 +0530

    setting component modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import styled from "styled-components";
 import NavBar from "./Components/Includes/NavBar";
 import Header from "./Components/Includes/Header";
 import Demo from "./Components/Screens/Demo";
+import Echart from "./Components/Screens/Echart";
+import MailBox from "./Components/Screens/MailBox";
+import Setting from "./Components/Screens/Setting";
+import Pages from "./Components/Screens/Pages";
 
 
 function App() {
@@ -14,6 +18,10 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Demo />} />
+          <Route path="/echart" element={<Echart />} />
+          <Route path="/mailbox" element={<MailBox />} />
+          <Route path="/settings" element={<Setting />} />
+          <Route path="/pages" element={<Pages />} />
         </Routes>
       </Conatiner>
     </Router>
---
+++ src/Components/Screens/MailBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MailBox() {
+  return (
+    <div>
+      <h3 style={{color:"white"}}>Coming Soon....</h3>
+    </div>
+  );
+}
+
+export default MailBox;
